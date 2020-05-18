8d6b6dbe56f56542a823acd90d257ca4d4c81279
Daniil <ddzevelyuk@mail.ru>
2020-05-18 10:12:49 +0300

    Произведён переход на стили MahApps MetroFramework

---
+++ DzevelyukCurs/Models/Breed.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzevelyukCurs.Models
+{
+    // Класс, описывающий таблицу "Породы".
+    public class Breed
+    {
+        // Свойства класса.
+        public int Id { get; set; } // Идентификатор.
+        public string Title { get; set; } // Название.
+        public int Performance { get; set; } // Среднее количество яиц в месяц.
+        public double AverageWeight { get; set; } // Средний вес.
+        public virtual Diet Diet { get; set; } // Рекомендованная диета.
+        // Сведения о породе включают в себя: название породы,
+        // среднее количество яиц в месяц(производительность)
+        // и средний вес, номер рекомендованной диеты.
+    } // Breed.
+}
---
+++ DzevelyukCurs/Models/Diet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzevelyukCurs.Models
+{
+    // Класс, описывающий таблицу "Диеты".
+    public class Diet
+    {
+        // Свойства класса.
+        public int Id { get; set; }                            // Идентификатор.
+        public string DietTitle { get; set; }                  // Название диеты.
+        public virtual ICollection<Breed> Breeds { get; set; } // Породы, которым рекомендована данная диета.
+    } // Diet.
+}
